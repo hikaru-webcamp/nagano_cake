6e84d2327e870a55e9de463a60eee50282c65d38
hikaru-webcamp <skyvader0524@gmail.com>
2021-05-16 23:21:48 +0000

    [add] 管理者TOPページ追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,14 +1,20 @@
 class Admin::HomesController < ApplicationController
 
 #ログイン済のカスタマーのみにアクセスを許可する
-  #before_action :authenticate_admin!
+  before_action :authenticate_admin!
+
+  #遷移元によって、処理を変える（要件記載）
+  #会員詳細から遷移した場合は、その顧客のみの注文履歴を表示
+  #ヘッダーから遷移してきた場合は、全顧客の注文履歴
 
   def top
     path = Rails.application.routes.recognize_path(request.referer)
-
+    #もし遷移元のコントローラーがadmin/customersであり、かつアクションがshowだったら
     if path[:controller]=="admin/customers" && path[:action] == "show"
+    #遷移してきたIDをカスタマーIDに入れて、whereで取得
       @orders = Order.where(customer_id: path[:id])
     else
+    #オーダーのデーター全部
       @orders = Order.all
     end
   end
