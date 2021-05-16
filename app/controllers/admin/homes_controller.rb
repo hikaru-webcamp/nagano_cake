972b74c40b90b69c72486b445facb566c0421d74
hikaru-webcamp <skyvader0524@gmail.com>
2021-05-16 08:45:16 +0000

    [add] 管理者ログイン画面追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,15 @@
 class Admin::HomesController < ApplicationController
+
+#ログイン済のカスタマーのみにアクセスを許可する
+  #before_action :authenticate_admin!
+
   def top
+    path = Rails.application.routes.recognize_path(request.referer)
+
+    if path[:controller]=="admin/customers" && path[:action] == "show"
+      @orders = Order.where(customer_id: path[:id])
+    else
+      @orders = Order.all
+    end
   end
 end
