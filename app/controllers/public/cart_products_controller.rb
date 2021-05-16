f724631e54510f5a52824015f4cc4323f2efe6b6
snaruse389 <s.naruse9337@gmail.com>
2021-05-16 14:59:25 +0000

    [add]カートに入れる機能

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -3,6 +3,14 @@ class Public::CartProductsController < ApplicationController
   end
 
   def create
+      cart_product = CartProduct.new(cart_product_params)
+      cart_product.customer_id = current_customer.id
+    if cart_product.quantity != 0
+      cart_product.save
+      redirect_to cart_products_path
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def update
@@ -12,5 +20,11 @@ class Public::CartProductsController < ApplicationController
   end
 
   def destroy_all
+  end
+  
+  private
+  
+  def cart_product_params
+    params.require(:cart_product).permit(:product_id, :quantity, :customer_id)	
   end
 end
