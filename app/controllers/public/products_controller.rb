3f4ca9a0c8b72ddc984d7f02ce3142f7ab6d9572
s.naruse9337@gmail.com <ec2-user@ip-172-31-23-174.ec2.internal>
2021-05-16 05:52:27 +0000

    [add]商品詳細の画面とshowアクションのα版1

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -3,5 +3,9 @@ class Public::ProductsController < ApplicationController
   end
 
   def show
+    # 商品情報の取得
+    @product = Product.find(params[:id])
+    # form_withのための空のインスタンス
+    @cart_product = CartProduct.new
   end
 end
