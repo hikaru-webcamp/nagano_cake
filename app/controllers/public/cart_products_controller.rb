b8db8004c3c3c9184a1413e949e4940d119174c2
snaruse389 <s.naruse9337@gmail.com>
2021-05-17 05:53:33 +0000

    [update]商品詳細とカートに追加　レビューの修正

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -3,10 +3,9 @@ class Public::CartProductsController < ApplicationController
   end
 
   def create
-      cart_product = CartProduct.new(cart_product_params)
-      cart_product.customer_id = current_customer.id
-    if cart_product.quantity != 0
-      cart_product.save
+    cart_product = CartProduct.new(cart_product_params)
+    cart_product.customer_id = current_customer.id
+    if cart_product.save
       redirect_to cart_products_path
     else
       redirect_back(fallback_location: root_path)
@@ -25,6 +24,6 @@ class Public::CartProductsController < ApplicationController
   private
   
   def cart_product_params
-    params.require(:cart_product).permit(:product_id, :quantity, :customer_id)	
+    params.require(:cart_product).permit(:product_id, :quantity)	
   end
 end
