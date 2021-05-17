3e0da5233675e9874632342417ee681c3b0c1015
hikaru-webcamp <skyvader0524@gmail.com>
2021-05-17 10:48:01 +0000

    [add] 商品一覧と商品登録機能追加

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,14 +1,23 @@
 class Admin::ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product.id)
+    else
+      render :new
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
@@ -16,4 +25,10 @@ class Admin::ProductsController < ApplicationController
 
   def update
   end
+
+  private
+  def product_params
+    params.require(:product).permit(:image, :name, :introduction, :price, :is_active, :genre_id, )
+  end
+
 end
