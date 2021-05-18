2945454f9ae0f6a9e2b7495454c176c38b9f49cb
Seiryu-alt <seiryusain@gmail.com>
2021-05-18 07:22:23 +0000

    [add] 製作ステータスの更新処理を追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,12 +4,9 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(params[:id])
-    if @order.update(order_params)
-      redirect_to admin_order_path(@order)
-    else
-      render :show
-    end
+    order = Order.find(params[:id])
+    order.update(order_params)
+    redirect_to admin_order_path(order)
   end
 
   private
