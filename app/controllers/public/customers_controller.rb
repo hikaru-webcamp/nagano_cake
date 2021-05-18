bd4818043ec2b6b68baf0326e33e222a379bcc31
haaanyan <kiha.0327@icloud.com>
2021-05-18 12:19:44 +0000

    [Fix]コントローラー修正 [Fix]リンク記述修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -26,11 +26,9 @@ class Public::CustomersController < ApplicationController
   def out_confirm
   end
   
-  
-  
   private
   def customer_params
     params.require(:customer).permit(
-      :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :tel, :email, :is_deleted)
+      :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :tel, :email )
   end
 end
