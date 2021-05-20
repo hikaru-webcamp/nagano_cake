0592ecf93c36d844a90a2bd9e530cf707274769c
snaruse389 <s.naruse9337@gmail.com>
2021-05-20 03:18:37 +0000

    [update]サインアップ、ログイン、ログアウト後のフラッシュメッセージの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    case resource
-    when Admin
-      admin_root_path
-    when Customer
-      flash[:notice] = "ながのCAKEへようこそ"
-      my_page_customers_path
-    end
-  end
 end
