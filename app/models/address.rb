ba9622b260eab869d69354b05466139a9c8a1be4
haaanyan <kiha.0327@icloud.com>
2021-05-21 09:14:53 +0000

    [add]エラーメッセージの日本語化

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ApplicationRecord
   
   NUMBER_REGEXP = /\A[0-9]+\z/
   
-  validates :postal_code, presence: true, format: { with: NUMBER_REGEXP, message: "郵便番号は半角数字のみで入力して下さい" }
+  validates :postal_code, presence: true, format: { with: NUMBER_REGEXP, message: "は半角数字のみで入力して下さい" }
   validates :address, presence: true
   validates :address_name, presence: true
 
