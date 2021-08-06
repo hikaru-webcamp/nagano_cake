a17d3b3b272a73984af321123b323df404cd06cd
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-06 11:37:27 +0000

    "first"

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(
-  email: 'admin@gmail.com',
-  password: 'teamsmile'
+  email: "admin@gmail.com",
+  password: "teamsmile",
 )
 
 20.times do |n|
@@ -21,7 +21,7 @@ Admin.create!(
     address: "テストデータ住所#{n}",
     tel: "0800000#{n}",
     email: "user#{n}@test.com",
-    password: "testtest"
+    password: "testtest",
   )
 end
 
@@ -31,14 +31,14 @@ Customer.all.each do |customer|
       customer_id: customer.id,
       address_name: "#{customer.last_name}#{customer.first_name}の住所#{n}",
       address: "#{customer.address}-#{n}",
-      postal_code: "0000#{customer.id}#{n}"
+      postal_code: "0000#{customer.id}#{n}",
     )
   end
 end
 
 5.times do |n|
   Genre.create!(
-    name: "テストジャンル#{n}"
+    name: "テストジャンル#{n}",
   )
 end
 
@@ -48,8 +48,8 @@ Genre.all.each do |genre|
       genre_id: genre.id,
       name: "テスト商品#{genre.id}-#{n}",
       introduction: "テスト商品#{genre.id}-#{n}の説明",
-      price: 100*rand(1..20),
-      image: File.open("#{Rails.root}/app/assets/images/test_image.png")
+      price: 100 * rand(1..20),
+      image: File.open("#{Rails.root}/app/assets/images/cake_images/cake_image#{n + 1}.jpg"),
     )
   end
 end
@@ -63,12 +63,12 @@ Customer.all.each do |customer|
       order_detail = OrderDetail.new(
         product_id: product.id,
         quantity: rand(1..5),
-        tax_included_price: product.tax_price
+        tax_included_price: product.tax_price,
       )
       total_price += order_detail.quantity * order_detail.tax_included_price
       order_details.push(order_detail)
     end
-    
+
     total_price += 800
 
     order = Order.create!(
@@ -76,9 +76,9 @@ Customer.all.each do |customer|
       shipping_fee: 800,
       total_price: total_price,
       payment_method: rand(0..1),
-      address_name: customer.last_name+customer.first_name,
+      address_name: customer.last_name + customer.first_name,
       address: customer.address,
-      postal_code: customer.postal_code
+      postal_code: customer.postal_code,
     )
 
     order_details.each do |order_detail|
