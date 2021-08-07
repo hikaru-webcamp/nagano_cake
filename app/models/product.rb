b7b9471e54de25773bbb84c5c5a1357b639108d3
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-07 06:24:08 +0000

    写真追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,17 +3,16 @@ class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :order_details, dependent: :destroy
 
-#画像アップ用のメソッド（attachment）を追加
+  #画像アップ用のメソッド（attachment）を追加
   attachment :image
 
-#バリデーションの記述(空でないこと)
+  #バリデーションの記述(空でないこと)
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
-  validates :image, presence: true
-  
+
   # 税込の計算
   def tax_price
-     # 税率
+    # 税率
     tax_rate = 1.08
     tax_price = self.price * tax_rate
     return tax_price.floor
