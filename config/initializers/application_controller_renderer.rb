7ae1f7664444820c220071f147e9c25772c90922
hikaru-webcamp <skyvader0524@gmail.com>
2021-08-07 02:30:34 +0000

    Refile エラー 記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#以下を追記
+Refile.secret_key = "a162701772e63591a54bc94c2debe004ae187b974b6bab176523f1810f474fe2afb3ea2a5f67c46af03f7f0a8fc61f008d055a94ac549c7c6e213255b3c360be"
